37c4c5dc2bc88ac987cfc4b79d5686ea2ffac79d
Chris Mahr <mahrtian@gmail.com>
2016-01-20 13:17:06 -0500

    save changes after moving seeds file

--- Gemfile
+++ Gemfile
@@ -2,9 +2,9 @@
 source "https://rubygems.org"
 
 # gem "rails"
-gem 'rake'
-gem 'sinatra'
-gem 'sinatra-contrib'
 gem 'pg'
 gem 'pry'
+gem 'rack'
+gem 'sinatra'
+gem 'sinatra-contrib'
 gem 'redcarpet'
--- lib/seeds.rb
+++ lib/seeds.rb
@@ -1,6 +1,15 @@
-require "pg"
+require 'pg'
 
-conn = PG.connect(dbname: "project2")
+if ENV["RACK_ENV"] == 'production'
+	conn = PG.connect(
+		dbname: ENV["POSTGRES_DB"], 
+		host: ENV["POSTGRES_HOST"], 
+		password: ENV["POSTGRES_PASS"], 
+		user: ENV["POSTGRES_USER"]
+	)
+else
+	conn = PG.connect(dbname: "project2")
+end	
 
 conn.exec("CREATE TABLE users (
 	id SERIAL PRIMARY KEY,
