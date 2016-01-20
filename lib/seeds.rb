86e3930af40d314242499d10373aed5261692fdf
Chris Mahr <mahrtian@gmail.com>
2016-01-20 13:11:02 -0500

    save ENV changes to seeds.rb file

--- lib/seeds.rb
+++ lib/seeds.rb
@@ -1,15 +1,6 @@
 require "pg"
 
-if ENV["RACK_ENV"] == 'production'
-	conn = PG.connect(
-		dbname: ENV["POSTGRES_DB"], 
-		host: ENV["POSTGRES_HOST"], 
-		password: ENV["POSTGRES_PASS"], 
-		user: ENV["POSTGRES_USER"]
-	)
-else
-	conn = PG.connect(dbname: "project2")
-end	
+conn = PG.connect(dbname: "project2")
 
 conn.exec("CREATE TABLE users (
 	id SERIAL PRIMARY KEY,
