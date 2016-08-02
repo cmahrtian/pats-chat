c35e45ce7954ff674fbac8e8ccb1cc5d4d62b7b7
Chris Mahr <mahrtian@gmail.com>
2016-08-02 10:10:20 -0400

    removed hidden comments and fixed indentations

--- server.rb
+++ server.rb
@@ -11,32 +11,29 @@ module Forum
 
 		enable :sessions
 
-			if ENV["RACK_ENV"] == 'production'
-				@@db ||= PG.connect(
-					dbname: ENV["POSTGRES_DB"], 
-					host: ENV["POSTGRES_HOST"], 
-					password: ENV["POSTGRES_PASS"], 
-					user: ENV["POSTGRES_USER"]
-				)
-			else
-				@@db = PG.connect(dbname: "pats_forum")
-			end
+		if ENV["RACK_ENV"] == 'production'
+			@@db ||= PG.connect(
+				dbname: ENV["POSTGRES_DB"], 
+				host: ENV["POSTGRES_HOST"], 
+				password: ENV["POSTGRES_PASS"], 
+				user: ENV["POSTGRES_USER"]
+			)
+		else
+			@@db = PG.connect(dbname: "pats_forum")
+		end
 
 		def current_user
-			if session["user_id"]
-				
+			if session["user_id"]	
 				@user ||= @@db.exec_params(<<-SQL, [session["user_id"]]).first
 					SELECT * FROM users WHERE id = $1
 				SQL
 			else 
-				# THE USER IS NOT LOGGED IN
 				{}	
 			end
 		end
 
 		get "/" do
 			@user = current_user
-			# @topics = @@db.exec_params("SELECT * FROM users JOIN topics on users.id = topics.id ORDER BY topic_score DESC")
 			@topics = @@db.exec_params("SELECT * FROM users JOIN topics on users.id = topics.user_id ORDER BY topic_score DESC")
 			# ABOVE IS THE PROPER CODE FOR HEROKU
 			erb :index
@@ -48,15 +45,12 @@ module Forum
 		end
 
 		post "/topic" do
-			markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, filter_html: true)
-    		
+			markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, filter_html: true)    		
 			@user = current_user
 			topic = params[:topic]
 			comment = markdown.render(params["comment"])
-
 			@@db.exec_params("INSERT INTO topics (topic_name, topic_comment, user_id) VALUES ($1, $2, $3)", [topic, comment, @user['id']])
 			@topic_comment = @@db.exec_params("SELECT * FROM topics WHERE topic_comment = $1", [comment]).first
-
 			@contact_submitted = true
 			erb :topic
 		end
@@ -67,27 +61,22 @@ module Forum
 			@topic = @@db.exec_params("SELECT * FROM topics WHERE id = $1", [@id]).first
 			@comments = @@db.exec_params("SELECT * FROM comments JOIN topics ON comments.topic_id = topics.id WHERE topics.id = #{@id}")
 			@poster = @@db.exec_params("SELECT * FROM users WHERE id = #{@topic['user_id']}").first
-			# @commenter = @@db.exec_params("SELECT * FROM comments JOIN users on comments.user_id = users.id WHERE comments.topic_id = #{@id}")
 			erb :topic_id
 		end
 
 		get "/topic/:id/comment" do
 			@user = current_user
 			@id = params[:id]
-			
 			erb :comment
 		end
 
 		post "/topic/:id/comment" do
 			markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, filter_html: true)
-    		
 			@user = current_user
 			@id = params[:id]
 			comment = markdown.render(params["comment"])
-
 			@@db.exec_params("INSERT INTO comments (comment_content, topic_id, user_id) VALUES ($1, $2, $3)", [comment, @id, @user['id']])
 			@@db.exec_params("UPDATE topics SET num_comments = num_comments + 1 WHERE id = #{@id.to_i}")
-
 			@comment_submitted = true
 			erb :comment
 		end
@@ -113,7 +102,6 @@ module Forum
 
 		post "/signup" do
 			encrypted_password = BCrypt::Password.create(params[:password])
-			
 			begin
 			  users = @@db.exec_params(<<-SQL, [params[:name],params[:email],encrypted_password, params[:avatar]])
 				INSERT INTO users (name, email, password, avatar) VALUES ($1, $2, $3, $4) RETURNING id; 
@@ -121,7 +109,6 @@ module Forum
 			rescue
 			  redirect '/signup?e=1'
 			end  		
-			
 			session["user_id"] = users.first['id']
 			redirect "/"
 			erb :index
@@ -161,7 +148,6 @@ module Forum
 			@comments = @@db.exec_params("SELECT * FROM comments WHERE user_id = #{@id}")
 			erb :profile
 		end
-
+		
 	end
-
 end
